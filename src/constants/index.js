bfe468fcbf7e91afaa0a462b5585705fa43fa0e1
Ebi Abaje <eabaje@outlook.com>
2022-01-24 13:38:56 +0100

    1-22-2022-3

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const SIGN_UP = "/register";
 //export const LOG_IN = "http://localhost:3000/signin";
-export const LOG_IN = "https://load-dispatch-admin.herokuapp.com";
+export const LOG_IN = "https://load-dispatch-admin.vercel.app";
 
 export const API_URL = "https://loadboard-api.herokuapp.com/api/";
 //export const API_URL = "http://localhost:8000/api/";
